03d89b371f1b13f89165c6e561a22e1f3296c453
Hemant Shrivastava <hemant@newput.com>
2021-05-27 23:18:50 +0530

    added login UI fix

--- FriendBook/src/assets/customLogin.js
+++ FriendBook/src/assets/customLogin.js
@@ -1,15 +1,16 @@
-// var box = document.querySelector('.container');
-// var boxRect = box.getBoundingClientRect();
+var box2 = document.querySelector('.container');
+var box = document.querySelector('.parent');
+var boxRect = box.getBoundingClientRect();
 
-// box.addEventListener('mousemove', e => {
-//   var xPosition = (e.clientX - boxRect.left) / boxRect.width;
-//   var yPosition = (e.clientY - boxRect.top) / boxRect.height - 0.8;
-//   var xOffset = -(xPosition - 0.8);
-//   var dxNorm = Math.min(Math.max(xOffset, -0.8), 0.8)
-//   box.style.transform = `perspective(1000px)
-//                         rotateY(${dxNorm*45}deg)
-//                         rotateX(${yPosition*45}deg) `
-//   box.addEventListener('mouseleave', () => {
-//     box.style.transform = 'skew'
-//   })
-// })
+box.addEventListener('mousemove', e => {
+  var xPosition = (e.clientX - boxRect.left) / boxRect.width;
+  var yPosition = (e.clientY - boxRect.top) / boxRect.height - 0.8;
+  var xOffset = -(xPosition - 0.8);
+  var dxNorm = Math.min(Math.max(xOffset, -0.8), 0.8)
+  box2.style.transform = `perspective(1000px)
+                        rotateY(${dxNorm*10}deg)
+                        rotateX(${yPosition*10}deg) `
+  box.addEventListener('mouseleave', () => {
+    box2.style.transform = 'skew'
+  })
+})
